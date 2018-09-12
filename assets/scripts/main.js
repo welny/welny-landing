ba3f3e4f6ccc2d303537f6e81729945fcbfab11d
OlegOschepkov <36763883+OlegOschepkov@users.noreply.github.com>
2018-09-12 23:21:16 +0400

    Update 2018-09-12T19:21:12.343Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,62 +2,27 @@
 
 // const slides = Array.from(document.querySelectorAll('.swiper-slide'));
 
-// function remove_style(all) {
-//   var i = all.length;
-//   var j, is_hidden;
+const menuBtn = document.querySelector('.js-toggle');
+const headerList = document.querySelector('.header-menu-list');
 
-//   // Presentational attributes.
-//   var attr = [
-//     'align',
-//     'background',
-//     'bgcolor',
-//     'border',
-//     'cellpadding',
-//     'cellspacing',
-//     'color',
-//     'face',
-//     'height',
-//     'hspace',
-//     'marginheight',
-//     'marginwidth',
-//     'noshade',
-//     'nowrap',
-//     'valign',
-//     'vspace',
-//     'width',
-//     'vlink',
-//     'alink',
-//     'text',
-//     'link',
-//     'frame',
-//     'frameborder',
-//     'clear',
-//     'scrolling',
-//     'style'
-//   ];
+menuBtn.addEventListener('click', function () {
+  if (headerList.classList.contains('visually-hidden')) {
+    headerList.classList.remove('visually-hidden');
+    menuBtn.classList.remove('menu-closed');
+    menuBtn.classList.add('menu-opened');
+  } else {
+    headerList.classList.add('visually-hidden');
+    menuBtn.classList.add('menu-closed');
+    menuBtn.classList.remove('menu-opened');
+  }
+});
 
-//   var attr_len = attr.length;
-
-//   while (i--) {
-//     is_hidden = (all[i].style.display === 'none');
-
-//     j = attr_len;
-
-//     while (j--) {
-//       all[i].removeAttribute(attr[j]);
-//     }
-
-//     // Re-hide display:none elements,
-//     // so they can be toggled via JS.
-//     if (is_hidden) {
-//       all[i].style.display = 'none';
-//       is_hidden = false;
-//     }
-//   }
-//   console.log('ok');
-
-// }
-
-// remove_style(slides);
+if (window.screen.width >= 1440) {
+  headerList.classList.remove('visually-hidden');
+} else {
+  if (!headerList.classList.contains('visually-hidden')) {
+    headerList.classList.add('visually-hidden');
+  }
+}
 
 /* eslint-enable */
