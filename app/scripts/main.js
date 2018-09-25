0192dcf8fed414e00566b4f0d56f480180945e81
OlegOschepkov <36763883+OlegOschepkov@users.noreply.github.com>
2018-09-25 23:10:53 +0400

    Исправления от заказчика - 1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,7 @@
 
 const menuBtn = document.querySelector('.js-toggle');
 const headerList = document.querySelector('.header-menu-list');
+const accordion = document.querySelector(".accordion");
 
 window.onresize = function () {
   if (window.innerWidth >= 1200) {
@@ -17,6 +18,14 @@ if (window.innerWidth >= 1200) {
   headerList.classList.remove('visually-hidden');
 } else if (window.innerWidth < 1200 & !headerList.classList.contains('visually-hidden')) {
   headerList.classList.add('visually-hidden');
+}
+
+if (window.innerWidth >= 1200) {
+  if (!accordion.classList.contains("is-active")) {
+    accordion.classList.add("is-active")
+  }
+} else {
+  accordion.classList.remove("is-active")
 }
 
 menuBtn.addEventListener('click', function () {
