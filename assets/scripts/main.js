5cbee26bdce39dc03f5b87290b2f0ffb2f23d86e
OlegOschepkov <36763883+OlegOschepkov@users.noreply.github.com>
2018-10-02 23:38:37 +0400

    Update 2018-10-02T19:38:30.536Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,21 @@
 /* eslint-disable */
 
-const menuBtn = document.querySelector('.js-toggle');
-const headerList = document.querySelector('.header-menu-list');
-const accordion = document.querySelector(".accordion");
-const sliderTop = document.querySelector('.slider-wrapper');
-const slider1 = document.querySelector(".swiper-wrapper");
-const slides = slider1.querySelectorAll('.swiper-slide')
-const titles = document.querySelectorAll('.wrapper-question-title');
-const answers = document.querySelectorAll('.answers');
-const orders = document.querySelectorAll('.order');
-const images = document.querySelectorAll('.img-wrapper')
-const blocksArr = Array.from(orders).concat(Array.from(titles)).concat(Array.from(answers)).concat(Array.from(images));
+'use strict';
+
+var menuBtn = document.querySelector('.js-toggle');
+var headerList = document.querySelector('.header-menu-list');
+var accordion = document.querySelector(".accordion");
+var sliderTop = document.querySelector('.slider-wrapper');
+var slider1 = document.querySelector(".swiper-wrapper");
+var slides = slider1.querySelectorAll('.swiper-slide');
+var titles = document.querySelectorAll('.wrapper-question-title');
+var answers = document.querySelectorAll('.answers');
+var orders = document.querySelectorAll('.order');
+var images = document.querySelectorAll('.img-wrapper');
+var blocksArr = Array.from(orders).concat(Array.from(titles)).concat(Array.from(answers)).concat(Array.from(images));
 
 function toggleSlides(className) {
-  blocksArr.forEach(element => {
+  blocksArr.forEach(function (element) {
     if (element.classList.contains(className) && element.classList.contains('visually-hidden')) {
       element.classList.remove('visually-hidden');
     } else if (!element.classList.contains(className) && !element.classList.contains('visually-hidden')) {
@@ -22,19 +24,19 @@ function toggleSlides(className) {
   });
 };
 
-sliderTop.addEventListener('click', function () {
-  let element = slider1.querySelector('.swiper-slide-active');
-  let subElement = element.querySelector('.slider__slide');
+sliderTop.addEventListener('DOMAttrModified', function () {
+  var element = slider1.querySelector('.swiper-slide-active');
+  var subElement = element.querySelector('.slider__slide');
   if (subElement.classList.contains('js-sport')) {
-    toggleSlides('js-sport')
+    toggleSlides('js-sport');
   } else if (subElement.classList.contains('js-anti')) {
-    toggleSlides('js-anti')
+    toggleSlides('js-anti');
   } else if (subElement.classList.contains('js-limfo')) {
-    toggleSlides('js-limfo')
+    toggleSlides('js-limfo');
   } else if (subElement.classList.contains('js-limfo2')) {
-    toggleSlides('js-limfo2')
+    toggleSlides('js-limfo2');
   } else if (subElement.classList.contains('js-classic')) {
-    toggleSlides('js-classic')
+    toggleSlides('js-classic');
   }
 });
 
@@ -54,10 +56,10 @@ if (window.innerWidth >= 1200) {
 
 if (window.innerWidth >= 1200) {
   if (!accordion.classList.contains("is-active")) {
-    accordion.classList.add("is-active")
+    accordion.classList.add("is-active");
   }
 } else {
-  accordion.classList.remove("is-active")
+  accordion.classList.remove("is-active");
 }
 
 menuBtn.addEventListener('click', function () {
