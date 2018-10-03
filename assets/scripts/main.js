f2674bf8ce7ef9243c8e0b087543a3106eb1d7ab
OlegOschepkov <36763883+OlegOschepkov@users.noreply.github.com>
2018-10-03 13:01:09 +0400

    Update 2018-10-03T09:01:02.991Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -86,7 +86,7 @@ menuBtn.addEventListener('click', function () {
 $(window).bind('resize', function (e) {
   if (window.RT) clearTimeout(window.RT);
   window.RT = setTimeout(function () {
-    this.location.reload(false); /* false to get page from cache */
+    document.location.reload(false); /* false to get page from cache */
   }, 100);
 });
 
